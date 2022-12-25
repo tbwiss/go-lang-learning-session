1b45decd4d445cdaf0c31457d3ce80555922d69f
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-12-25 14:30:26 +0100

    feat: full test

--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Tom")
-	want := "Hello, Tom"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Tom")
+		want := "Hello, Tom"
 
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello to world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
