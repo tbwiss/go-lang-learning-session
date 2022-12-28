febd61b8ef68c1f9b53d032474dd7c5c893e9622
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-12-28 11:33:56 +0100

    feat: struct and methods

---
+++ structs/struct.go
@@ -0,0 +1,28 @@
+package structs
+
+import "math"
+
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Height * rectangle.Width
+}
