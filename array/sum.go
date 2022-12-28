8e9e4e84d478de7679964c4daf748105e634334e
Thomas Wiss <thomas.wiss@neotechnology.com>
2022-12-28 10:23:39 +0100

    feat: sumAll

--- array/sum.go
+++ array/sum.go
@@ -1,9 +1,20 @@
 package array
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// Here: https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/arrays-and-slices#write-the-test-first-1
--- array/sum_test.go
+++ array/sum_test.go
@@ -1,16 +1,28 @@
 package array
 
 import "testing"
+import "reflect"
 
 func TestSum(t *testing.T) {
-	t.Run("sum as array", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("sum as array, array size 5", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
 		sum := Sum(numbers)
 		want := 15
 
 		if sum != want {
 			t.Errorf("sum %d, want %d, given %v", sum, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sum as array, array size 5", func(t *testing.T) {
+		sum := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(sum, want) {
+			t.Errorf("sum %v, want %v", sum, want)
 		}
 	})
 }
